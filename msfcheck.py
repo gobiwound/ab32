ecd2f178333ee372920ba7b4f598c34799be5729
gobiwound <118014051+gobiwound@users.noreply.github.com>
2022-11-14 08:18:22 -0800

    Update msfcheck.py

--- msfcheck.py
+++ msfcheck.py
@@ -1,23 +1,25 @@
 #Metasploit is the world's most used penetration testing software.
 # Uncover weaknesses in your defenses, focus on the right risks, and improve security.
 #https://www.rapid7.com/products/metasploit/
+#checking the metasploit modules path
 import subprocess
 from msfpath import check
 def version():
-    cmd = "uname"
+    osfind = "uname"
     try:
-        check = subprocess.call([cmd, '-a'],
+        check = subprocess.call([osfind, '-a'],
                                 stdout=subprocess.DEVNULL,
                                 stderr=subprocess.STDOUT)
     except:
         print()
 def check_path():
-    cmd = "which"
+    osfind = "which"
     try:
-        check=subprocess.call([cmd, 'msfvenom'],
+        check=subprocess.call([osfind, 'msfvenom'],
     stdout=subprocess.DEVNULL,
     stderr=subprocess.STDOUT)
     except:
         print ()
 check_path()
 version()
+#https://github.com/rapid7/metasploit-framework
